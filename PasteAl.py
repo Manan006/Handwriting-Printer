0385c736acdd83fa56b4c2940333c23c78032bb6
Manan006 <58476411+Manan006@users.noreply.github.com>
2020-09-11 20:41:37 +0530

    Add files via upload

---
+++ PasteAl.py
@@ -0,0 +1,42 @@
+inp=input("Enter Text: ")
+#inp="Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna"
+#inp="The post has just arrived and in it a very nice surprise, the discovery that Jacques Seguela, one-time adviser to President Mitterrand, now close confidant of President and Madame Sarkozy (indeed he intoduced them), and something of a legend in French political communications, has dedicated his latest book to little old moi.z"
+#inp="Look at this, This has been created using the handwriter printer(version 1.0). Have a look and tell me if it feels handwritten. If it doesn't feel handwritten then PLEASE TELL me how i can make it feel more handwritten. Thanks"
+#inp="Tamil population believed that the administration was not sympathetic about Tamil linguistics, their education, and culture. Tamil population observed victimized because they were not given preference for government jobs."
+
+
+
+
+
+
+
+
+
+
+
+
+#from PIL import Image
+#
+#        Aal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Apng")
+#        Cal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Cpng")
+#        Dal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Dpng")
+#        Eal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Epng")
+#        Fal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Fpng")
+#        Gal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Gjpg")
+#        Hal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Hpng")
+#        Ial = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Ijpg")
+#        Jal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Jpng")
+#        Kal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Kjpg")
+#        Lal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Lpng")
+#        Mal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Mjpg")
+#        Nal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Npng")
+#        Oal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Opng")
+#        Pal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Ppng")
+#        Qal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Qpng")
+#        Sal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Spng")
+#        Tal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Tpng")
+#        Ual = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Upng")
+#        Val = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Vpng")
+#        Xal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Xpng")
+#        Yal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Ypng")
+#        Zal = Imageopen(r"C:\Users\home\PycharmProjects\Alphabets\Zjpg")
